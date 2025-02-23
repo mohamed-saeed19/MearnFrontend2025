d2030f1b4ce9a5b8f214df839f2c86d2499bdc44
Mohamed Abo Saeed <133701978+mohamed-saeed19@users.noreply.github.com>
2025-02-23 02:44:28 +0200

    lab

--- src/app/lab/messages/messages.lab.component.spec.ts
+++ src/app/lab/messages/messages.lab.component.spec.ts
@@ -1,10 +1,40 @@
-
+import { TestBed } from "@angular/core/testing";
+import { ComponentFixture } from "@angular/core/testing";
+import { MessagesComponentForLab } from "./messages.lab.component";
+import { MessageService } from "../../services/message/message.service";
+import { Component } from "@angular/core";
 
 describe("2-message component integration testing:", () => {
-   
-   
-    it("expect component template to be empty", () => {
-        //Note: there is @if"messageService.messages.length" in line 1 in template
-    })
-    it("then expect div.msg to have the messages after setting it", () => {})
-})
+    let component: MessagesComponentForLab;
+    let fixture: ComponentFixture<MessagesComponentForLab>;
+    let messageService: MessageService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [MessagesComponentForLab],
+            providers: [MessageService],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(MessagesComponentForLab);
+        component = fixture.componentInstance;
+        messageService = TestBed.inject(MessageService);
+        fixture.detectChanges();
+    });
+
+    it("expect component template to be empty when no messages exist", () => {
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector("div.msg")).toBeNull();
+    });
+
+    it("then expect div.msg to have the messages after setting it", () => {
+        messageService.messages = ["Test Message 1", "Test Message 2"];
+        fixture.detectChanges();
+
+        const compiled = fixture.nativeElement;
+        const messagesDiv = compiled.querySelectorAll("div.msg");
+
+        expect(messagesDiv.length).toBe(2);
+        expect(messagesDiv[0].textContent).toContain("Test Message 1");
+        expect(messagesDiv[1].textContent).toContain("Test Message 2");
+    });
+});
--- src/app/lab/pipe/square.pipe.spec.ts
+++ src/app/lab/pipe/square.pipe.spec.ts
@@ -1,6 +1,21 @@
-describe("1-square pipe (class only) testing:",()=>{
+import { SquarePipeForLab } from "./square.pipe";
 
-    it("expect to return 16 when passing 4",()=>{})
-    it("expect to return 9 when passing '3'",()=>{})
-    it("expect to return 'Not a number' when passing wrong parameter",()=>{})
-})
+describe("1-square pipe (class only) testing:", () => {
+    let pipe: SquarePipeForLab;
+
+    beforeEach(() => {
+        pipe = new SquarePipeForLab();
+    });
+
+    it("expect to return 16 when passing 4", () => {
+        expect(pipe.transform(4)).toBe(16);
+    });
+
+    it("expect to return 9 when passing '3'", () => {
+        expect(pipe.transform("3")).toBe(9);
+    });
+
+    it("expect to return 'Not a number' when passing wrong parameter", () => {
+        expect(pipe.transform("abc")).toBe("not a number");
+    });
+});
